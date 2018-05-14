9c36d8ff4af33c5abc43c1bc1e7d4ea68dd601ec
Pavel <p_evdokimov@chebit24.ru>
2018-05-14 20:54:55 +0300

    init

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author student
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.printf("hello world\n");
+        int a = 101;
+        
+        System.out.println("a = " + a);
+        
+        float b;
+        b = a + (float)2.6;
+        b = b / 0;
+        System.out.println("b = " + b);
+        
+    }
+    
+}
